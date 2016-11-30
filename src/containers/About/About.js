132a57c30a47a393d1f83c763fd0613e28e8706f
KuongKnight <kuongknight.bk@gmail.com>
2016-11-30 14:57:16 +0700

    Remove for kit

--- src/containers/About/About.js
+++ src/containers/About/About.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import Helmet from 'react-helmet';
-import { MiniInfoBar } from 'components';
 
 export default class About extends Component {
 
@@ -17,32 +16,6 @@ export default class About extends Component {
       <div className="container">
         <h1>About Us</h1>
         <Helmet title="About Us"/>
-
-        <p>This project was originally created by Erik Rasmussen
-          (<a href="https://twitter.com/erikras" target="_blank">@erikras</a>), but has since seen many contributions
-          from the open source community. Thank you to <a
-            href="https://github.com/erikras/react-redux-universal-hot-example/graphs/contributors"
-            target="_blank">all the contributors</a>.
-        </p>
-
-        <h3>Mini Bar <span style={{color: '#aaa'}}>(not that kind)</span></h3>
-
-        <p>Hey! You found the mini info bar! The following component is display-only. Note that it shows the same
-          time as the info bar.</p>
-
-        <MiniInfoBar/>
-
-        <h3>Images</h3>
-
-        <p>
-          Psst! Would you like to see a kitten?
-
-          <button className={'btn btn-' + (showKitten ? 'danger' : 'success')}
-                  style={{marginLeft: 50}}
-                  onClick={this.handleToggleKitten}>
-            {showKitten ? 'No! Take it away!' : 'Yes! Please!'}</button>
-        </p>
-
         {showKitten && <div><img src={kitten}/></div>}
       </div>
     );
